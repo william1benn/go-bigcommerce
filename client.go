33f9653802120a43f9e4b594977a4eab7b22c32f
seanomeara96 <seanom96@gmail.com>
2023-05-25 08:43:41 +0100

    added new client func

--- client.go
+++ client.go
@@ -1,13 +1,25 @@
 package bigcommerce
 
 import (
+	"log"
 	"net/http"
 	"net/url"
 )
 
 type Client struct {
 	BaseURL    *url.URL
-	UserAgent  string
+	AuthToken  string
 	httpClient *http.Client
 }
 
+func NewClient(version string, storeHash string, authToken string) *Client {
+	var client Client
+	url, err := url.Parse("https://api.bigcommerce.com/stores/" + storeHash + "/v" + version)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client.BaseURL = url
+	client.AuthToken = authToken
+	client.httpClient = http.DefaultClient
+	return &client
+}
