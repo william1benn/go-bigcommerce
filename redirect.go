a3a5f22765b5c801aa07d126c321a16866ef20d2
seanomeara96 <seanom96@gmail.com>
2023-06-06 22:04:20 +0100

    added get all redirects function

--- redirect.go
+++ redirect.go
@@ -1,5 +1,7 @@
 package bigcommerce
 
+import "encoding/json"
+
 type Redirect struct {
 	ID       int      `json:"id"`
 	SiteID   int      `json:"site_id"`
@@ -12,4 +14,46 @@ type ToObject struct {
 	Type     string `json:"type"`
 	EntityID int    `json:"entity_id"`
 	URL      string `json:"url"`
+}
+
+func (client *Client) GetRedirects(params RedirectQueryParams) ([]Redirect, error) {
+	type ResponseObject struct {
+		Data []Redirect `json:"data"`
+		Meta MetaData   `json:"meta"`
+	}
+	var response ResponseObject
+
+	queryParams, err := paramString(params)
+	if err != nil {
+		return response.Data, err
+	}
+
+	getRedirectsURL := client.BaseURL.JoinPath("/storefront/redirects").String() + queryParams
+
+	resp, err := client.Request("GET", getRedirectsURL)
+	if err != nil {
+		return response.Data, err
+	}
+	defer resp.Body.Close()
+
+	if err = expectStatusCode(200, resp); err != nil {
+		return response.Data, err
+	}
+
+	if err = json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return response.Data, err
+	}
+
+	return response.Data, nil
+}
+
+type RedirectQueryParams struct {
+	SiteID    int    `url:"ite_id,omitempty"`
+	IDs       []int  `url:"id,omitempty"`
+	Limit     int    `url:"limit,omitempty"`
+	Page      int    `url:"page,omitempty"`
+	Sort      string `url:"sort,omitempty"`
+	Direction string `url:"direction,omitempty"`
+	Include   string `url:"include,omitempty"`
+	Keyword   string `url:"keyword,omitempty"`
 }
