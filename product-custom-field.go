58c5d3a260a245a86ab5b2556aad46b17d912b5a
seanomeara96 <seanom96@gmail.com>
2023-06-08 21:22:56 +0100

    can get custom fields

--- product-custom-field.go
+++ product-custom-field.go
@@ -1,20 +1,55 @@
 package bigcommerce
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type ProductCustomField struct {
 	ID    int    `json:"id"`
 	Name  string `json:"name"`
 	Value string `json:"value"`
 }
 
-func GetCustomFields(productID int, params ProductCustomFieldsRequestParams) {
+func (client *Client) GetCustomFields(productID int, params ProductCustomFieldsRequestParams) (ProductCustomField, error) {
+	type ResponseObject struct {
+		Data ProductCustomField `json:"data"`
+		Meta MetaData           `json:"meta"`
+	}
+	var response ResponseObject
 	// /catalog/products/{product_id}/custom-fields
+	queryString, err := paramString(params)
+	if err != nil {
+		return response.Data, err
+	}
+
+	getCustomFieldPath := client.BaseURL.JoinPath("/catalog/products", fmt.Sprint(productID), "/custom-fields").String() + queryString
+
+	resp, err := client.Get(getCustomFieldPath)
+	if err != nil {
+		return response.Data, err
+	}
+	defer resp.Body.Close()
+
+	err = expectStatusCode(200, resp)
+	if err != nil {
+		return response.Data, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return response.Data, err
+	}
+
+	return response.Data, nil
 }
-func CreateCustomFields(productID int, params CreateCustomFieldParams) {}
-func GetCustomField(productID int, customFieldID int) {
+func (client *Client) CreateCustomFields(productID int, params CreateCustomFieldParams) {}
+func (client *Client) GetCustomField(productID int, customFieldID int) {
 	// /catalog/products/{product_id}/custom-fields/{custom_field_id}
 }
-func UpdateCustomField(productID int, customFieldID int, params UpdateCustomFieldParams) {}
-func DeleteCustomField(productID int, customFieldID int)                                 {}
+func (client *Client) UpdateCustomField(productID int, customFieldID int, params UpdateCustomFieldParams) {
+}
+func (client *Client) DeleteCustomField(productID int, customFieldID int) {}
 
 type ProductCustomFieldsRequestParams struct {
 	IncludeFields string `url:"include_fields,omitempty"`
