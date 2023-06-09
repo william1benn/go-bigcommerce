aa1b07ceda0e27dc40b2245611a449501bff2011
seanomeara96 <seanom96@gmail.com>
2023-06-09 08:10:39 +0100

    can convert product into variantcreate params

--- conversions.go
+++ conversions.go
@@ -7,6 +7,30 @@ package bigcommerce
 	first port of call is the ability to convert  from a product to a variant
 */
 // TODO I actuall need to convert to a CreateProductVariantParams object...
-func (product *Product) ToVariant(productID int) ProductVariant {
-	return ProductVariant{}
+func (product *Product) ToVariantCreateParams(parentProductID int) ProductVariantCreateParams {
+	return ProductVariantCreateParams{
+		CostPrice:              product.CostPrice,
+		Price:                  product.SalePrice,
+		RetailPrice:            product.RetailPrice,
+		Weight:                 product.Weight,
+		Width:                  product.Width,
+		Height:                 product.Height,
+		Depth:                  product.Depth,
+		IsFreeShipping:         product.IsFreeShipping,
+		FixedCostShippingPrice: product.FixedCostShippingPrice,
+		UPC:                    product.UPC,
+		InventoryLevel:         product.InventoryLevel,
+		InventoryWarningLevel:  product.InventoryWarningLevel,
+		BinPickingNumber:       product.BinPickingNumber,
+		GTIN:                   product.GTIN,
+		MPN:                    product.MPN,
+		ProductID:              parentProductID,
+		SKU:                    product.SKU,
+	}
+}
+
+func (product *Product) ToVariantCreateParamsWithOptions(parentProductID int, options []VariantOption) ProductVariantCreateParams {
+	createVariantParams := product.ToVariantCreateParams(parentProductID)
+	createVariantParams.OptionValues = &options
+	return createVariantParams
 }
