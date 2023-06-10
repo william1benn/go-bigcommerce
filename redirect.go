a94bd3749cbf7799a167aa270ccae5a1658cd285
seanomeara96 <seanom96@gmail.com>
2023-06-10 23:21:33 +0100

    can upsert redirects

--- redirect.go
+++ redirect.go
@@ -1,6 +1,9 @@
 package bigcommerce
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"errors"
+)
 
 type Redirect struct {
 	ID       int              `json:"id"`
@@ -56,6 +59,87 @@ type RedirectQueryParams struct {
 	Direction string `url:"direction,omitempty"`
 	Include   string `url:"include,omitempty"`
 	Keyword   string `url:"keyword,omitempty"`
+}
+
+func validateRedirectUpsert(redirect RedirectUpsert) error {
+	if redirect.FromPath == "" {
+		return errors.New("from_path is required")
+	}
+
+	if redirect.SiteID <= 0 {
+		return errors.New("site_id must be a positive integer")
+	}
+
+	if redirect.To.Type == "" {
+		return errors.New("to.type is required")
+	}
+
+	if redirect.To.Type != "product" && redirect.To.Type != "brand" && redirect.To.Type != "category" &&
+		redirect.To.Type != "page" && redirect.To.Type != "post" && redirect.To.Type != "url" {
+		return errors.New("to.type has an invalid value")
+	}
+
+	if redirect.To.Type != "url" && redirect.To.EntityID <= 0 {
+		return errors.New("to.entity_id must be a positive integer")
+	}
+
+	if redirect.To.Type == "url" && len(redirect.To.URL) > 2048 {
+		return errors.New("to.url must be 2048 characters or less")
+	}
+
+	return nil
+}
+
+type RedirectUpsert struct {
+	FromPath string         `json:"from_path"`
+	SiteID   int            `json:"site_id"`
+	To       RedirectTarget `json:"to"`
+}
+
+type RedirectTarget struct {
+	Type     string `json:"type"`
+	EntityID int    `json:"entity_id"`
+	URL      string `json:"url"`
+}
+
+func (client *Client) UpsertRedirects(redirects []RedirectUpsert) ([]Redirect, error) {
+	type ResponseObject struct {
+		Data []Redirect `json:"data"`
+		Meta MetaData   `json:"meta"`
+	}
+	var response ResponseObject
+
+	for i := 0; i < len(redirects); i++ {
+		err := validateRedirectUpsert(redirects[i])
+		if err != nil {
+			return response.Data, err
+		}
+	}
+
+	paramBytes, err := json.Marshal(redirects)
+	if err != nil {
+		return response.Data, err
+	}
+
+	path := client.BaseURL.JoinPath("/storefront/redirects").String()
+
+	resp, err := client.Put(path, paramBytes)
+	if err != nil {
+		return response.Data, err
+	}
+	defer resp.Body.Close()
+
+	err = expectStatusCode(201, resp)
+	if err != nil {
+		return response.Data, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return response.Data, err
+	}
+
+	return response.Data, nil
 }
 
 type DeleteRedirectsParams struct {
